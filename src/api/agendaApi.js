e5e531ef1422b80d7766e1e9c992aea721eb6532
minhochoi <yovnthf1@naver.com>
2018-01-22 20:19:17 +0900

    Add sample code use axios

--- src/api/agendaApi.js
+++ src/api/agendaApi.js
@@ -1,8 +1,9 @@
+import axios from 'axios'
 const STORAGE_KEY = 'temp-grass-fe'
 
 export function fetchAgendaList () {
-    return Promise.resolve({ 
-        status: 200, 
+    return Promise.resolve({
+        status: 200,
         data: {
             list: [...JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')]
         }
@@ -10,8 +11,8 @@ export function fetchAgendaList () {
 }
 
 export function fetchAgenda(id) {
-    return Promise.resolve({ 
-        status: 200, 
+    return Promise.resolve({
+        status: 200,
         data: JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')[id]
     });
 }
@@ -21,14 +22,24 @@ export function createAgenda(agenda) {
     agenda.id = agendaList.length;
     agendaList.push(agenda)
     localStorage.setItem(STORAGE_KEY, JSON.stringify(agendaList))
-    return Promise.resolve({ 
+    return Promise.resolve({
         status: 200
     });
 }
 
+export function getGunni() {
+  axios.get('/user?ID=12345')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
 export default {
-  uid: 0, 
+  uid: 0,
   fetchAgendaList,
-  fetchAgenda, 
+  fetchAgenda,
   createAgenda
 }
