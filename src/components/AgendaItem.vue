e627c4fdac4a71f4d1af62cb46063549f39d1ba1
amazingguni <amazingguni@gmail.com>
2018-02-05 21:59:26 +0900

    Init agendaitem file

---
+++ src/components/AgendaItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <router-link to="/agenda" tag="div">
+        {{ agenda.title }}
+    </router-link>
+</template>
+<script>
+  export default {
+      props: ['agenda'],
+  }
+</script>
+<style>
+</style>
--- src/components/AgendaListView.vue
+++ src/components/AgendaListView.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <div v-for="agenda in agendaList" :key="agenda.id">{{agenda.title}}</div>
-  </div>
+  <main>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            안건목록
+          </h1>
+          <agenda-item v-for="agenda in agendaList" :key="agenda.id" :agenda="agenda"></agenda-item>    
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 <script>
-
   import {mapState} from 'vuex'
   import {ACTION} from '../store/config'
-
+  import agendaItem from './AgendaItem'
   export default {
+    components: {agendaItem},
     computed: mapState([
       'agendaList'
     ])
